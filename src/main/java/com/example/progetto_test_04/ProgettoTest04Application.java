c238786c855a5e98dca9d42824ee68ee2cd4cb38
DESKTOP-QB9SM4A\leler <lelerizzo3@gmail.com>
2022-12-07 19:02:08 +0100

    primo commit

---
+++ src/main/java/com/example/progetto_test_04/ProgettoTest04Application.java
@@ -0,0 +1,13 @@
+package com.example.progetto_test_04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoTest04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoTest04Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/progetto_test_04/ProgettoTest04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.progetto_test_04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoTest04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
